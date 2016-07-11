e2dd4a461c390279ff1f658295c1ada0cb6d3706
Rithanya27 <rithanya27@gmail.com>
2016-07-11 09:50:29 +0530

    Update hanoi.c

--- hanoi.c
+++ hanoi.c
@@ -2,19 +2,20 @@
 #include<conio.h>
 void main()
 {
-void change(int,int,int,int);
+void tow(int,int,int,int);
 int disk=3;
 clrscr();
 printf("move to the disk");
-change(disk,1,3,2);
+tow(disk,1,3,2);
 getch();
 }
-void change(int c,int inti,int final,int t)
+void tow(int c,int inti,int final,int t)
 {
 if(c>0)
 {
-change(c-1,inti,t,final);
+tow(c-1,inti,t,final);
 printf("new dis %d from %d to %d\n",c,inti,final);
-change(c-1,t,final,inti);
+tow(c-1,t,final,inti);
+prinf("result")
 }
 }
